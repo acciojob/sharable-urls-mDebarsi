852c175ef2f3532c1e2f21a2594a785e50067c92
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2024-02-12 22:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// your code here
-function generateURL(){
-    const name = document.getElementById('name').value;
-    const year = document.getElementById('year').value;
-    let url = 'https://localhost:8080/';
-    if(name.length + year.length > 0) url += '?';
-    if(name!='') url += 'name=' + name;
-    if(year!=''){
-      if(name!='') url += '&'
-      url += 'year=' + year;
-    }
-    document.getElementById('url').innerHTML = url;
-  }
-  document.getElementById('button').addEventListener('click', generateURL);
+document.getElementById("urlForm").addEventListener("submit", function(event) {
+            event.preventDefault(); 
+            
+            var name = document.getElementById("name").value;
+            var year = document.getElementById("year").value;
+            
+            var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+            
+            document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+        });
